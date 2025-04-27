f099a329b790a11e9f0b6b9797fd63dabcfc24c6
MinervaCC <cascorreia96@gmail.com>
2025-04-27 19:22:14 +0100

    .

--- src/Communication/Command.java
+++ src/Communication/Command.java
@@ -3,6 +3,7 @@ package Communication;
 // Enumera os comandos para a troca das mensagens entre o cliente e o servidor
 
 public enum Command {
+    ConnectionRequest,
     String,
     Terminate,
     FileSearchResult,
--- src/Communication/NewConnectionRequest.java
+++ src/Communication/NewConnectionRequest.java
@@ -3,20 +3,18 @@ package Communication;
 import java.io.Serializable;
 
 public class NewConnectionRequest implements Serializable {
-    private String senderHost;
-    private int senderPort;
-
-    public NewConnectionRequest(String senderHost, int senderPort) {
-        this.senderHost = senderHost;
-        this.senderPort = senderPort;
+    private String ip;
+    private int port;
+    public NewConnectionRequest(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
     }
 
-    public String getSenderHost() {
-        return senderHost;
+    public String getIp() {
+        return ip;
     }
 
-    public int getSenderPort() {
-        return senderPort;
+    public int getPort() {
+        return port;
     }
 }
-
